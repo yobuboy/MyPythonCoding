06cfcadbb49f6b55d56a7510c422e973cca88743
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-09-12 17:12:42 +0800

    Challenge by kuya Kim

--- challenge2.py
+++ challenge2.py
@@ -1,6 +1,7 @@
 ent = input("Enter Number: ")
-if ent.isdigit():
+if ent.isdigit():#(.isdigit) Declare's that the text is int, not a str.
     ent = int(ent)
     print(ent % 10)
 else:
+
     print("Error")
